06ffc226c97d3c90b4845bfa559dc2fa3d6f10ee
Cooper <eliteswimmer42@gmail.com>
2020-08-29 11:34:35 -0500

    filling in html structures to replace with js

--- script.js
+++ script.js
@@ -10,15 +10,41 @@
 // 
 
 
+
+
+
+//
+//
+
+
+// https://materializecss.com/range.html
+// calorie counter in scale form; isn't appearing for whatever reason
+var slider = document.getElementById('test-slider');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   orientation: 'horizontal', // 'horizontal' or 'vertical'
+   range: {
+     'min': 1200,
+     'max': 6000,
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
+
+
 // function that runs upon clicking a foodPreference checkbox
 // function takes the boxVal of the checkbox clicked and either adds or removes it
+// tbh idk how to assign values to checkboxes lmao but not actually lmao
 $("#checkbox").on("click", function (boxVal) {
     $.ajax({
         url: `https://api.edamam.com/search?q=${boxVal}&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db`,
-        method: "GET"
+        method: "GET",
     })
-    console.log()
-})
+    console.log("https://api.edamam.com/search?q=dinner&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db")
+});
 
 
 // 
