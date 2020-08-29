5aa0fe3300054b1873e6b6d4b67e2ae626877ba0
Cooper <eliteswimmer42@gmail.com>
2020-08-29 10:22:00 -0500

    plotting a basic checkbox function

--- script.js
+++ script.js
@@ -8,8 +8,23 @@
 /////////////////////////////////////////////////////////////////////
 // coop's section
 // 
+
+
+// function that runs upon clicking a foodPreference checkbox
+// function takes the boxVal of the checkbox clicked and either adds or removes it
+$("#checkbox").on("click", function (boxVal) {
+    $.ajax({
+        url: `https://api.edamam.com/search?q=${boxVal}&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db`,
+        method: "GET"
+    })
+    console.log()
+})
+
+
 // 
-// hello
+//
+
+
 //
 //
 //
