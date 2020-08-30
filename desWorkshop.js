1ee057b0738a0ac57b4e83f3beccc4102ddc559b
Desmond <desmondaldridge@gmail.com>
2020-08-29 19:52:18 -0700

    Almost have the ajax call built

--- desWorkshop.js
+++ desWorkshop.js
@@ -1,10 +1,29 @@
+
+// If your BMI is...	You are...
+// Under 18.5	Underweight
+// Between 18.5 and 24.9	Normal
+// Between 25 and 29.9	Overweight
+// 30 and Over	Obese
+
+var breakfast = "breakfast";
+var lunch = "lunch";
+var dinner = "dinner";
+
+var underweight = 0000; // Suggested daily calories
+var normal = 0000;
+var overweight = 0000;
+var obese = 0000;
+
+
+// FULL url "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to={3 OR LAST INDEX OF YOUR CHOICE}calories={591-722 CALORIE RANGE PER CATEGORY} {&health=alcohol-free}"
+
 var apiURL = "https://api.edamam.com/search?q="
 var apiID = "&app_id=d88e78b8"
 var apiKey = "&app_key=fa1417bd0cb262ef6b1af85af54b21db"
 
-var searchKeyword = ""
+var whichMeal = "";
 
-var queryURL = apiURL + searchKeyword + apiID + apiKey
+var queryURL = apiURL + whichMeal + apiID + apiKey;
 
 $("button").click(function(){
     $.ajax({url: queryURL, method: "GET" success: function(result){
