eac86c19e247bdcc660a9497797125ba16981863
Desmond <desmondaldridge@gmail.com>
2020-08-30 19:05:52 -0700

    Git-ting a lot done... git what I did there? ... and there...?

--- desWorkshop.js
+++ desWorkshop.js
@@ -1,48 +1,30 @@
 
-// If your BMI is...	You are...
-// Under 18.5	Underweight
-// Between 18.5 and 24.9	Normal
-// Between 25 and 29.9	Overweight
-// 30 and Over	Obese
-
-var breakfast = "breakfast";
-var lunch = "lunch";
-var dinner = "dinner";
-
-// var bmiUnderweight = 00; 
-// var bmiNormal = 00;
-// var bmiOverweight = 00;
-// var bmiObese = 00;
-
-// Input Gender
-// Input Weight
-// Input Height
-// Input Age
-
 let myGender= ""; // From User Input
-let myHeight= 0; // From User Input
+let myHeightFt= 0; // From User Input
+let myHeightIn= 0; // From User Input
 let myWeight= 0; // From User Input
 let myAge = 0; // From User Input
 
-var heightInches = myHeight / 12;
-var genderLetter = //trim&lowercase myGender to first letter
+var convertFeetToInches = myHeightFt / 12;
+var heightFull = convertFeetToInches + myHeightIn;
+// var genderLetter = myGender.string.charAt(0).toLowercase(); // Change this, no typing, just selection
 
 function getBMR(gender) {
     if (gender == "m") {
         
-        66 + (6.2 * myWeight) + (12.7 * heightInches) - (6.76 * myAge);
+        66 + (6.2 * myWeight) + (12.7 * heightFull) - (6.76 * myAge);
     }
-    if (gender == "w") {
+    if (gender == "f") {
         
-        655 + (4.35 * myWeight) + (4.7 * heightInches) - (4.7 * myAge);
+        655 + (4.35 * myWeight) + (4.7 * heightFull) - (4.7 * myAge);
     }
 }
 
-var myBMR = getBMR(genderLetter);
+var myBMR = getBMR(myGender);
 
 var myActivityLevel = ""; // From User Input
 
-// clickevent buttons to push value of letter or num representing three options: 0.sedentary 1.light to moderate, 2.active, or 3.vigorous(athlete or manual laborer) to empty array
+// click event buttons to push value of letter or num representing three options: 0.sedentary 1.light to moderate, 2.active, or 3.vigorous(athlete or manual laborer) to empty array
 
 function getCalories(BMR) {
 
@@ -63,29 +45,83 @@ function getCalories(BMR) {
 var myCaloricIntake = getCalories(myBMR);
 
 
-
-
 // Maintain Calorie Range
-var calorieRange1 = parseInt(myCaloricIntake * .2); 
-var calorieRange2 = parseInt(myCaloricIntake * .3); 
+var calorieRange1 = Math.round(myCaloricIntake * .2); 
+var calorieRange2 = Math.round(myCaloricIntake * .3); 
 
-// var calorieRangeFULL = calorieRange1 + calorieRange2;
-
-
+var calorieRangeFULL = "calories=" + calorieRange1 + "-" + calorieRange2;
 
 
+
+// AJAX Call
 // FULL url "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to={3 OR LAST INDEX OF YOUR CHOICE}calories={591-722 CALORIE RANGE PER CATEGORY} {&health=alcohol-free}"
 
-var apiURL = "https://api.edamam.com/search?q="
-var apiID = "&app_id=d88e78b8"
-var apiKey = "&app_key=fa1417bd0cb262ef6b1af85af54b21db"
+var apiURL = "https://api.edamam.com/search?q=";
+var apiID = "&app_id=d88e78b8";
+var apiKey = "&app_key=fa1417bd0cb262ef6b1af85af54b21db";
+var resultAmount = "&from=0&to=10"; // This will show 10 
 
 var whichMeal = "";
 
-var queryURL = apiURL + whichMeal + apiID + apiKey + "&from=0&to=10" + "calories=" + calorieRange1 + "-" + calorieRange2;
+var queryURL = apiURL + whichMeal + apiID + apiKey + resultAmount + calorieRangeFULL;
 
 $("button").click(function(){
-    $.ajax({url: queryURL, method: "GET" success: function(result){
+    $.ajax({url: queryURL, method: "GET", success: function(result){
       $("#div1").html(result);
     }});
+});
+
+
+  // Height Selection
+  // Utilizing a new click event method for submit buttons within html forms
+  // The target needs to be the form id, not the input id
+
+  var myFeet = $("#feet-input").value;
+  var myInches = $("#inches-input").value;
+ 
+
+  // Feet
+  $("#feet-form").submit(function(event) {
+    myHeightFt.push(myFeet);
+    event.preventDefault();
   });
+
+  // Inches
+  $("#inches-form").submit(function(event) {
+    myHeightIn.push(myInches);
+    event.preventDefault();
+  });
+
+
+  // Gender Selection
+  $("#maleBTN").click(function(event) {
+    event.preventDefault();
+  
+      myGender.push("m");
+
+    });
+
+  $("#femaleBTN").click(function(event) {
+    event.preventDefault();
+    
+      myGender.push("f");
+
+    });
+
+    //var pastSearches = [];
+
+// City Search
+// $("#select-city").click(function(event) {
+//     event.preventDefault();
+  
+//     var inputCity = $("#city-input").val().trim().toUpperCase();
+//     searchWeather(inputCity);
+  
+//     if (pastSearches.indexOf(inputCity) === -1) {
+//       pastSearches.push(inputCity);
+//     }
+  
+//     renderButtons();
+  
+  
+//   });
