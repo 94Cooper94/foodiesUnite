834be5c958f06f50587e0ad91f51687615670a31
Cooper <eliteswimmer42@gmail.com>
2020-09-03 00:23:08 -0500

    merging

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 ////////////////////////////////////////////////////////////////////
 // deezmond's section
+//
 // From User Input
 var myGender = [""]; // From User Input
 var myHeightFt = [];
@@ -191,7 +192,9 @@ $("#submit-button").click(function (event) {
 
     calorieRangeFULL = "&calories=" + calorieRange1 + "-" + calorieRange2;
     console.log(calorieRangeFULL);
-    $('#BMRval').append("Calorie Day Intake " + myCaloricIntake)
+
+    var calorieDisplay = myCaloricIntake;
+    $('#BMRval').append("caloric intake: " + calorieDisplay)
 
 
     // AJAX Call
@@ -231,99 +234,11 @@ $("#submit-button").click(function (event) {
     });
 });
 
-//var searchTerm = ""
-// var app_id = "d88e78b8";
-// var api_key = "fa1417bd0cb262ef6b1af85af54b21db";
-// var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=" + app_id + "&app_key=" + api_key;
-
-// $ajax({
-//   queryURL,
-//   type: 'GET',
-//   dataType: 'json',
-//   success: function (q) {
-//     console.log(q)
-//   }
-// })
 // 
-// 
-/////////////////////////////////////////////////////////////////////
-// coop's section
-// 
-// $("#preferenceDiv").append(dairyFree, glutenFree, pescatarian, vegan, vegetarian)
-// var dairyFree = $(document.createElement("input")).attr({
-//   id: 'dairy-free'
-//   , name: 'dairy-free'
-//   , value: '2'
-//   , text: 'Dairy-free'
-//   , type: 'checkbox'
-//   , checked: true
-// })
-// var glutenFree = $(document.createElement("input")).attr({
-//   id: 'gluten-free'
-//   , name: 'gluten-free'
-//   , value: '2'
-//   , text: 'Gluten-free'
-//   , type: 'checkbox'
-//   , checked: true
-// })
-// var pescatarian = $(document.createElement("input")).attr({
-//   id: 'pescatarian'
-//   , name: 'pescatarian'
-//   , value: '2'
-//   , text: 'Pescatarian'
-//   , type: 'checkbox'
-//   , checked: true
-// })
-// var vegan = $(document.createElement("input")).attr({
-//   id: 'vegan'
-//   , name: 'vegan'
-//   , value: '2'
-//   , text: 'Vegan'
-//   , type: 'checkbox'
-//   , checked: true
-// })
-// var vegetarian = $(document.createElement("input")).attr({
-//   id: 'vegetarian'
-//   , name: 'vegetarian'
-//   , value: '2'
-//   , text: 'Vegetarian'
-//   , type: 'checkbox'
-//   , checked: true
-// })
-
-
-
-// function that runs upon clicking a foodPreference checkbox
-// function takes the boxVal of the checkbox clicked and either adds or removes it
-// tbh idk how to assign values to checkboxes lmao but not actually lmao
-// $("#mealType").on("click", function (mealType) {
-//   $.ajax({
-//     url: `https://api.edamam.com/search?q=${mealType}&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db`,
-//     method: "GET",
-//   })
-//   console.log("https://api.edamam.com/search?q=dinner&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db")
-// });
-// $("#checkbox").on("click", function (boxVal) {
-//   $.ajax({
-//     url: `https://api.edamam.com/search?q=${boxVal}&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db`,
-//     method: "GET",
-//   })
-//   console.log("https://api.edamam.com/search?q=dinner&app_id=d88e78b8&app_key=fa1417bd0cb262ef6b1af85af54b21db")
-// });
-//
-//
-//
-////////////////////////////////////////////////////////////////////
-// justin's section
-//
-//Booty queen
-//
-//
-//
 //
 /////////////////////////////////////////////////////////////////////
 // austin's section
-
+//
 //for the BMI api
 
 $('#submit-button').click(function (num) {
@@ -360,17 +275,14 @@ $('#submit-button').click(function (num) {
         var bmiAnswers = show(response);
 
         $('#showBmi').html(bmiAnswers);
-
-
-
     });
 
 
     function show(response) {
         var bmiNum = response.bmi
         var healthNum = response.health
-        return "<h3><strong>bmi</strong>: " + bmiNum.toFixed(2) + "</h3>" +
-            "<h3><strong>health status</strong>: " + healthNum + "</h3>";
+        return "<h5>bmi: " + bmiNum.toFixed(2) + "</h5>" +
+            "<h5>health status: " + healthNum + "</h5>";
 
     }
 });
@@ -385,3 +297,16 @@ $('#submit-button').click(function (num) {
 //
 //
 //
+//var searchTerm = ""
+// var app_id = "d88e78b8";
+// var api_key = "fa1417bd0cb262ef6b1af85af54b21db";
+// var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=" + app_id + "&app_key=" + api_key;
+
+// $ajax({
+//   queryURL,
+//   type: 'GET',
+//   dataType: 'json',
+//   success: function (q) {
+//     console.log(q)
+//   }
+// })
